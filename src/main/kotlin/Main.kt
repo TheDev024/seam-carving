8801858d4af513976f846adb59d47f75e278448e
TheDev024 <thedeveloper024@gmail.com>
2023-02-18 19:50:20 +0400

    stage-1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,23 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import java.util.Scanner
+import javax.imageio.ImageIO
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val scanner = Scanner(System.`in`)
+
+fun main() {
+    println("Enter rectangle width:")
+    val width = scanner.nextInt() - 1
+    println("Enter rectangle height:")
+    val height = scanner.nextInt() - 1
+    println("Enter output image name:")
+    val outPath = scanner.next()
+    val outFile = File(outPath)
+    val image = BufferedImage(width + 1, height + 1, BufferedImage.TYPE_INT_BGR)
+    val graphic = image.graphics
+    graphic.color = Color.RED
+    graphic.drawLine(0, 0, width, height)
+    graphic.drawLine(0, height, width, 0)
+    ImageIO.write(image, "png", outFile)
 }
